8ea719bdced4a7239b3c9e0e916a9bc9a08752f1
gulsher <ahmed.gulsher@gmail.com>
2023-05-24 21:58:58 +0530

    graphql query for book and author added to server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
-const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLInt , GraphQLNonNull} = require('graphql');
 const app = express();
 const port = 5000;
 
@@ -15,16 +15,49 @@ const books = [{id:1, name: "harry 1",authorId:1},
 {id:7, name: "harry 7",authorId:3},
 {id:8, name: "harry 8",authorId:3}]
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({ 
-        name:"helloWorld",
-        fields:()=>({
-            message:{
-                type:GraphQLString,
-                resolve:() => 'Hello world'
-            }
-        })
+
+const BookType = new GraphQLObjectType({
+    name:'Books',
+    description:'this is a book',
+    fields:()=>({
+        id:{type:GraphQLNonNull(GraphQLInt)},
+        name:{type:GraphQLNonNull(GraphQLString)},
+        authorId:{type:GraphQLNonNull(GraphQLInt)},
+        author:{type:AuthorType,
+            resolve:(book)=>{
+            return authors.find(author=>author.id === book.authorId
+            )}}
     })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    description:'this is a author desc',
+    fields:()=>({
+        id:{type:GraphQLNonNull(GraphQLInt)},
+        name:{type:GraphQLNonNull(GraphQLString)},
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name:'Query',
+    description:'Root Query',
+    fields:()=>({
+        books:{
+            type:new GraphQLList(BookType),
+            description:'list of books',
+            resolve:()=> books //in real time application it would be database values
+        },
+        authors:{
+            type:new GraphQLList(AuthorType),
+            description:'list of author',
+            resolve:()=> authors //in real time application it would be database values
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
 })
 
 app.use('/graphql', graphqlHTTP({
